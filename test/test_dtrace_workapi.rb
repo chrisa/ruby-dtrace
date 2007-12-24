da5d0d7520a94b721c760e40010a395dc5466278
chrisandrews <chrisandrews@f5dbc166-08e2-44d3-850c-36126807790f>
2007-12-24 16:50:30 +0000

    Testing aggregates and consume_once
    git-svn-id: http://ruby-dtrace.rubyforge.org/svn/trunk@48 f5dbc166-08e2-44d3-850c-36126807790f

--- test/test_dtrace_workapi.rb
+++ test/test_dtrace_workapi.rb
@@ -112,6 +112,54 @@ EOD
    
   end
 
+  def test_work_dprogram_aggregates_once
+    t = Dtrace.new 
+    t.setopt("bufsize", "4m")
+    t.setopt("aggsize", "4m")
+
+    progtext = <<EOD
+ruby*:::function-entry
+{ 
+  @a[execname, copyinstr(arg1), copyinstr(arg2)] = count(); 
+}
+
+END
+{
+  printa(@a)
+}
+EOD
+
+    prog = t.compile progtext
+    prog.execute
+
+    t.go
+    
+    foo = 0
+    (1..1000).each do |i|
+      foo = foo + i
+    end
+    
+    c = DtraceConsumer.new(t)
+    
+    c.consume_once do |e|
+      assert e
+      case e.class.to_s
+      when "DtraceProbeData"
+        assert e.probedesc
+        e.each_record do |r|
+          assert r.value
+        end
+      when "DtraceRecord"
+        assert e.value
+      when "DtraceAggregate"
+        assert e.value
+        assert e.tuple
+        assert_equal 3, e.tuple.length
+      end
+    end
+    
+  end
+  
   def test_work_dprogram_once
     t = Dtrace.new 
     t.setopt("bufsize", "4m")
