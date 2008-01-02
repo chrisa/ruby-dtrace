5cb0ef234047d96676886babb30c9c9561a4f117
chrisandrews <chrisandrews@f5dbc166-08e2-44d3-850c-36126807790f>
2008-01-02 23:39:19 +0000

    Factor out the callbacks; add docs.
    git-svn-id: http://ruby-dtrace.rubyforge.org/svn/trunk@51 f5dbc166-08e2-44d3-850c-36126807790f

--- lib/dtraceconsumer.rb
+++ lib/dtraceconsumer.rb
@@ -3,6 +3,41 @@
 # (c) 2007 Chris Andrews <chris@nodnol.org>
 #
 
+# A DtraceConsumer provides access to the data produced by the running
+# D program. Having compiled and executed a D program, you typically
+# create a DtraceConsumer, and wait for data. 
+#
+# You can either wait indefinitely for data, or consume all the data
+# waiting and then stop: if your D program consists of only of
+# aggregations, returned by printa() actions in the END block, this
+# will be the best approach. If you have a mix of trace() and print()
+# actions elsewhere, you'll probably want to wait until interrupted,
+# the D program itself exits, or your program decides it has collected
+# enough data.
+#
+# The two approaches are implemented by the +consume+ and
+# +consume_once+ methods.
+#
+# The objects returned are of the following classes, depending on the
+# actions fired:
+#
+# * DtraceRecord
+# * DtraceAggregate
+# * DtraceStackRecord
+#
+# You must have already started tracing when you call +consume+ or
+# +consume_once+, so the general structure will look like:
+#
+#   t = Dtrace.new
+#   progtext = "..."
+#   prog = t.compile progtext
+#   prog.execute
+#   t.go 
+#   c = DtraceConsumer.new(t)
+#   c.consume_once do |r|
+#     # handle records
+#   end
+
 class DtraceConsumer
 
   def initialize(t)
@@ -10,91 +45,59 @@ class DtraceConsumer
     @curragg = DtraceAggregate.new
   end
 
-  def consume
+  private
+  
+  def probe_consumer(block)
+    proc do |probe|
+      # Handle multiple records in each Probedata
+      probe.each_record do |rec|
+        block.call(rec)
+      end
+    end
+  end
 
-    probe_consumer = proc do |probe|
-      yield probe
-    end
-    
-    rec_consumer = proc do |rec|
-      #yield rec
-    end
-    
-    buf_consumer = proc do |buf|
+  def buf_consumer(block)
+    proc do |buf|
       r = buf.record
       # buf records can be empty (trace();)
       if r 
         case r.class.to_s
         when DtraceStackRecord.to_s
-          yield r
+          block.call(r)
         when DtraceRecord.to_s
-          yield r
+          block.call(r)
         when DtraceAggData.to_s
-          case r.aggtype
-          when "tuple"
-            @curragg.tuple << r.value
-          when "value"
-            @curragg.value = r.value
-          when "last"
-            yield @curragg
+          if agg = @curragg.add_record(r)
+            block.call(agg)
             @curragg = DtraceAggregate.new
           end
-
         end
       end
-    end    
+    end
+  end
   
-    @t.go
-    @t.buf_consumer(buf_consumer)
+  public
+  
+  # Waits for data from the D program, and yields the records returned
+  # to the block given. Returns when the D program exits.
+  def consume(&block)
+    @t.buf_consumer(buf_consumer(block))
     begin
       while(true) do
         @t.sleep
-        @t.work(probe_consumer, rec_consumer)
+        @t.work(probe_consumer(block))
       end
     ensure
       @t.stop
-      @t.work(probe_consumer, rec_consumer)
+      @t.work(probe_consumer(block))
     end
-    
   end
   
-  def consume_once
-    
-    probe_consumer = proc do |probe|
-      probe.each_record do |r|
-        yield r
-      end
-    end
-    
-    rec_consumer = proc do |rec|
-      #yield rec
-    end
-    
-    buf_consumer = proc do |buf|
-      r = buf.record
-      # buf records can be empty (trace();)
-      if r 
-        case r.class.to_s
-        when DtraceRecord.to_s
-          yield r
-        when DtraceAggData.to_s
-          case r.aggtype
-          when "tuple"
-            @curragg.tuple << r.value
-          when "value"
-            @curragg.value = r.value
-          when "last"
-            yield @curragg
-            @curragg = DtraceAggregate.new
-          end
-
-        end
-      end
-    end
-  
-    @t.buf_consumer(buf_consumer)
+  # Yields the data waiting from the current program, then returns.
+  def consume_once(&block)
+    @t.buf_consumer(buf_consumer(block))
     @t.stop
-    @t.work(probe_consumer, rec_consumer)
+    @t.work(probe_consumer(block))
   end
   
 end
