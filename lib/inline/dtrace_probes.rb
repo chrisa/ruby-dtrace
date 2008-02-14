fda51ca4190bacba481286be145b4776cb476484
chrisandrews <chrisandrews@f5dbc166-08e2-44d3-850c-36126807790f>
2008-02-14 08:40:17 +0000

    Add an override to fix the RubyInline breakage that means it crashes if the bundle has already been built.
    git-svn-id: http://ruby-dtrace.rubyforge.org/svn/trunk@97 f5dbc166-08e2-44d3-850c-36126807790f

--- lib/inline/dtrace_probes.rb
+++ lib/inline/dtrace_probes.rb
@@ -10,6 +10,16 @@ end
 
 module Inline
   class DtraceProbes < Inline::C
+
+    # Overriding to fix the @rb_file breakage: For some reason the
+    # stock RubyInline leaves the line number on the filename, then it
+    # crashes trying to File.mtime it.
+    def rb_file
+      @rb_file.split(/:/)[0]
+    end
+
+    # Allow the module name to be set - normally you don't care, 
+    # but we do here because it shows up in the probe name.
     def set_module_name(name)
       @module_name = name
     end
