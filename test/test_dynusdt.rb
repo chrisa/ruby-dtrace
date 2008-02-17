54d36f8f6ec25cbbee33cb252c559067ee58170a
chrisandrews <chrisandrews@f5dbc166-08e2-44d3-850c-36126807790f>
2008-02-17 13:22:35 +0000

    Extend tests for Dtrace::Provider USDT probes.
    git-svn-id: http://ruby-dtrace.rubyforge.org/svn/trunk@103 f5dbc166-08e2-44d3-850c-36126807790f

--- test/test_dynusdt.rb
+++ test/test_dynusdt.rb
@@ -11,17 +11,16 @@ require 'rbconfig'
 # Tests for the Dtrace "dynamic USDT" library
 
 class TestDynUsdt < Test::Unit::TestCase
-  def test_create_usdt
 
-    Dtrace::Provider.create :test_provider do |p|
+  def test_create_usdt
+    Dtrace::Provider.create :test_provider_create_usdt do |p|
       p.probe :test_probe, :string
     end
 
     t = Dtrace.new
     t.setopt("bufsize", "4m")
-    t.setopt("aggsize", "4m")
 
-    progtext = 'test_provider*:::test-probe { trace(copyinstr(arg0)); }'
+    progtext = 'test_provider_create_usdt*:::test-probe { trace(copyinstr(arg0)); }'
 
     prog = t.compile progtext
     prog.execute
@@ -29,7 +28,7 @@ class TestDynUsdt < Test::Unit::TestCase
     
     c = DtraceConsumer.new(t)
     
-    Dtrace::Probe::TestProvider.test_probe do |p|
+    Dtrace::Probe::TestProviderCreateUsdt.test_probe do |p|
       p.fire "test_argument"
     end
 
@@ -38,12 +37,99 @@ class TestDynUsdt < Test::Unit::TestCase
       i = i + 1
       assert d
       assert_not_nil d.probe
-      assert_equal "test_provider#{$$.to_s}:test_provider.#{Config::CONFIG['DLEXT']}:test_probe:test-probe", d.probe.to_s
+      assert_equal "test_provider_create_usdt#{$$.to_s}:test_provider_create_usdt.#{Config::CONFIG['DLEXT']}:test_probe:test-probe", d.probe.to_s
       assert_equal 1, d.data.length
       assert_equal "test_argument", d.data[0].value
     end
     assert_equal 1, i
-    
   end
+
+  def test_create_usdt_argtypes
+    Dtrace::Provider.create :test_provider_create_usdt_argtypes do |p|
+      p.probe :test_probe_string, :string
+      p.probe :test_probe_int,    :integer
+    end
+
+    t = Dtrace.new
+    t.setopt("bufsize", "4m")
+
+    progtext = <<EOD
+test_provider_create_usdt_argtypes*:::test-probe-string { trace(copyinstr(arg0)); }
+test_provider_create_usdt_argtypes*:::test-probe-int    { trace(arg0);            }
+EOD
+
+    prog = t.compile progtext
+    prog.execute
+    t.go
+    
+    c = DtraceConsumer.new(t)
+    
+    Dtrace::Probe::TestProviderCreateUsdtArgtypes.test_probe_string do |p|
+      p.fire "test_argument"
+    end
+
+    Dtrace::Probe::TestProviderCreateUsdtArgtypes.test_probe_int do |p|
+      p.fire 42
+    end
+
+    i = 0
+    c.consume_once do |d|
+      assert d
+      assert_not_nil d.probe
+      if d.probe.name == 'test-probe-string'
+        assert_equal "test_argument", d.data[0].value
+        i = i + 1
+      elsif d.probe.name == 'test-probe-int'
+        assert_equal 42, d.data[0].value
+        i = i + 1
+      end
+    end
+    assert_equal 2, i
+  end
+
+  def test_create_usdt_manyargs
+    Dtrace::Provider.create :test_provider_create_usdt_manyargs do |p|
+      p.probe :test_probe, :string, :string, :string, :string, :string, :string, :string, :string
+    end
+
+    t = Dtrace.new
+    t.setopt("bufsize", "4m")
+
+    progtext = <<EOD
+test_provider_create_usdt_manyargs*:::test-probe 
+{ 
+  trace(copyinstr(arg0));
+  trace(copyinstr(arg1));
+  trace(copyinstr(arg2));
+  trace(copyinstr(arg3));
+  trace(copyinstr(arg4));
+  trace(copyinstr(arg5));
+  trace(copyinstr(arg6));
+  trace(copyinstr(arg7));
+}
+EOD
+
+    prog = t.compile progtext
+    prog.execute
+    t.go
+    
+    c = DtraceConsumer.new(t)
+    
+    Dtrace::Probe::TestProviderCreateUsdtManyargs.test_probe do |p|
+      values_list = (0..7).map {|i| "value #{i.to_s}" }
+      p.fire(*values_list)
+    end
+
+    i = 0
+    c.consume_once do |d|
+      i = i + 1
+      assert_equal 8, d.data.length
+      (0..7).each do |j|
+        assert_equal "value #{j.to_s}", d.data[j].value
+      end
+    end
+    assert_equal 1, i
+  end
+
 end
     
