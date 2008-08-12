70379f0ead18b297de674903c07ea91cd1b990fb
Chris Andrews <chris@nodnol.org>
2008-08-12 21:07:35 +0100

    Switch platform detection to shell out to uname -p -- Config::Config['target'] lies on Intel with Apple's Universal ruby.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,27 +2,28 @@ require 'mkmf'
 require 'rbconfig'
 
 def symlink(old, new)
-  if File.exists?(new) && File.symlink?(new)
+  begin
+    File.symlink(old, new)
+  rescue Errno::EEXIST
     File.unlink(new)
+    retry
   end
-  File.symlink(old, new)
 end
 
 $CFLAGS += " -D_LONGLONG_TYPE -g"
 have_library("dtrace", "dtrace_open")
 
-# Update machine-dependent symlinks in the source, based on $Config::CONFIG
-
-cpu = Config::CONFIG['target_cpu']
+# Update machine-dependent symlinks in the source, based on $Config::CONFIG and `uname -p`
 os  = Config::CONFIG['target_os']
-
-cpu.gsub! /^i[4-6]86/, 'i386'
 os.gsub!  /[0-9.]+$/, ''
+
+# On OSX, this is "powerpc", even on Intel...
+#cpu = Config::CONFIG['target_cpu']
+cpu = `uname -p`.chomp
 
 dir = "#{cpu}-#{os}"
 symlink "#{dir}/dtrace_probe.c", "dtrace_probe.c"
 
 # Create makefile in the usual way
-
 create_makefile("dtrace_api")
 
