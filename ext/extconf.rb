a228c86f7b83b5b65a59104b77557032fa9e6d38
Chris Andrews <chrisandrews@venda.com>
2011-02-01 08:51:29 +0000

    Remove -g from CFLAGS.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 require 'rbconfig'
 
-$CFLAGS += " -D_LONGLONG_TYPE -g"
+$CFLAGS += " -D_LONGLONG_TYPE"
 
 # Need to specify full path to dtrace.h or we'll pick up ruby's
 # dtrace.h on Solaris or other builds with the runtime probes included.
