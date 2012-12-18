2f611b882d147eae6553bb2503355a3032edc33d
Chris Andrews <chris@nodnol.org>
2012-12-18 08:36:55 +0000

    Don't fail if there's no ruby*::: probe.

--- test/test_rubyprobe.rb
+++ test/test_rubyprobe.rb
@@ -13,40 +13,40 @@ EOD
 
     begin
       prog = @dtp.compile progtext
+      prog.execute
+      @dtp.go
     rescue DTrace::Exception
-      flunk "no ruby probe"
+      nil
     end
-    prog.execute
-    @dtp.go
 
-    c = DTrace::Consumer.new(@dtp)
+    if prog
 
-    # Leopard's ruby-probe is DTracer, Solaris's is Tracer.
-    begin
-      trace_module = DTracer
-    rescue NameError
+      c = DTrace::Consumer.new(@dtp)
+
+      # Leopard's ruby-probe is DTracer, Solaris's is Tracer.
       begin
-        trace_module = Tracer
+        trace_module = DTracer
       rescue NameError
-        flunk "no DTracer or Tracer module"
+        begin
+          trace_module = Tracer
+        end
+      end
+
+      (0..9).each do |i|
+        trace_module.fire("foo", i.to_s)
+      end
+
+      data = []
+      c.consume_once do |d|
+        data << d
+      end
+
+      (0..9).each do |i|
+        d = data.shift
+        assert_equal("foo", d.data[0].value)
+        assert_equal(i.to_s, d.data[1].value)
       end
     end
-
-    (0..9).each do |i|
-      trace_module.fire("foo", i.to_s)
-    end
-
-    data = []
-    c.consume_once do |d|
-      data << d
-    end
-
-    (0..9).each do |i|
-      d = data.shift
-      assert_equal("foo", d.data[0].value)
-      assert_equal(i.to_s, d.data[1].value)
-    end
-
   end
 
 end
