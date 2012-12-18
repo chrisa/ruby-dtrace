f4d03edf54f3d5cf0695981e6c32992a3451686a
Chris Andrews <chris@nodnol.org>
2012-12-18 08:21:18 +0000

    Remove old Rails plugin

--- plugin/dtrace/init.rb
+++
@@ -1,7 +0,0 @@
-require 'dtrace_report'
-ActionController::Base.class_eval do
-  include DtraceReport
-  before_filter :enable_dtrace if RAILS_ENV == 'development'
-  after_filter :append_dtrace_report if RAILS_ENV == 'development'
-end
-
